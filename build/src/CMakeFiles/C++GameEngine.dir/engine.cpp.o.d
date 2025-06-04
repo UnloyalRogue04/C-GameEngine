e8894f90ac5c4eeaa6d23411300463b0a8029864
UnloyalRogue04 <peterjohn050@icloud.com>
2025-06-04 23:23:31 +0200

    Stop tracking build directory

--- build/src/CMakeFiles/C++GameEngine.dir/engine.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/C++GameEngine.dir/engine.cpp.o: \
-  /Users/peterjohngeorgekilian/github/C++GameEngine/src/engine.cpp \
-  /Users/peterjohngeorgekilian/github/C++GameEngine/include/engine.hpp \
-  /Users/peterjohngeorgekilian/github/C++GameEngine/include/mod_graphics/mod_graphics.hpp
--- build/src/mod_graphics/CMakeFiles/mod_graphics.dir/mod_graphics.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/mod_graphics/CMakeFiles/mod_graphics.dir/mod_graphics.cpp.o: \
-  /Users/peterjohngeorgekilian/github/C++GameEngine/src/mod_graphics/mod_graphics.cpp \
-  /Users/peterjohngeorgekilian/github/C++GameEngine/include/mod_graphics/mod_graphics.hpp
